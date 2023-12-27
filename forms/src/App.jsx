1fa796b285ff4e1785c53158342a41985c6b0451
AdityaPourush <adityapourush11@outlook.com>
2023-12-27 17:13:07 +0530

    done

---
+++ forms/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './Components/Forms'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <Form/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ forms/src/Components/Forms.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+
+const Form = () => {
+
+    const [formSubmit, setFormSubmit] = useState(false)
+    const [formError, setFormError] =  useState({})
+    const [formData, setFormData] = useState({
+        Email: "",
+        FirstName: "",
+        LastName: "",
+        PhoneNo: ""
+    })
+
+    // const handleFirstName = (e)=>{
+    //     setFormData({
+    //         ...formData,
+    //         firstName: e.target.value
+    //     })
+    // }
+
+    // const handleLastName = (e)=>{
+    //     setFormData({
+    //         ...formData,
+    //         lastName: e.target.value
+    //     })
+    // }
+
+    // const handleEmail = (e)=>{
+    //     setFormData({
+    //         ...formData,
+    //         email: e.target.value
+    //     })
+    // }
+
+    // const handlePhNo = (e)=>{
+    //     setFormData({
+    //         ...formData,
+    //         phone: e.target.value
+    //     })
+    // }
+
+    // console.log("formdata :" , formData);
+
+    const handleInputChange = e =>{
+        let{name, value} = e.target
+        setFormData({
+            ...formData,
+            [name]:value
+        })
+    }
+
+    // console.log("formdata :" , formData);
+
+    const handleFormSubmit = (e)=>{
+        e.preventDefault()
+        console.log(formData);
+
+        let errors = validate(formData)
+        // console.log('errors :', errors);
+        setFormError(errors)
+
+        let errKeyArray = Object.keys(errors)
+
+        if(errKeyArray.length == 0){
+            setFormSubmit(true)
+        }else{
+            setFormSubmit(false)
+        }
+    }
+
+    const validate = (data)=>{
+        let error = {}
+        if(data.FirstName.trim() == ""){
+            error.FirstName = "Please enter your First Name"
+        }
+        if(data.LastName.trim() == ""){
+            error.LastName = "Please enter your Last Name"
+        }
+        if(data.Email.trim() == ""){
+            error.Email = "Please enter your email"
+        }
+        if(data.PhoneNo.trim() == ""){
+            error.PhoneNo = "Please enter your Phone no."
+        }
+        if(data.PhoneNo.trim().length != 10){
+            error.PhoneNo = "Please enter 10 digit no."
+        }
+        return error;
+    }
+
+
+    return (
+        <>
+            <div style={{ textAlign: "center" }}>
+                <h1>Registration Form</h1>
+
+                <div className="form-container">
+                    <fieldset>
+                        <legend>Fill this form</legend>
+                        <form onSubmit={handleFormSubmit}>
+                            {formSubmit && <div className="success">
+                                <p>Registration Successfull</p>
+                            </div>}
+
+                            <label>First Name</label>
+                            <input type="text" name="FirstName" onChange={handleInputChange}/>
+                            {formError.FirstName && <p className="err">{formError.FirstName}</p>}
+
+                            <label>Last Name</label>
+                            <input type="text" name="LastName" onChange={handleInputChange}/>
+                            {formError.LastName && <p className="err">{formError.LastName}</p>}
+
+                            <label>Email</label>
+                            <input type="text" name="Email" onChange={handleInputChange}/>
+                            {formError.Email && <p className="err">{formError.Email}</p>}
+
+                            <label>Phone No.</label>
+                            <input type="number" name="PhoneNo" onChange={handleInputChange}/>
+                            {formError.PhoneNo && <p className="err">{formError.PhoneNo}</p>}
+
+                            <input type="Submit" id="submit-button" value={"Register"}/>
+                        </form>
+                    </fieldset>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Form;
